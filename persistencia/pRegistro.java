d882522b4e8dccdfeb9196895c669c669a5e7ae1
Andrews <andrewschiozo@gmail.com>
2016-06-01 18:04:44 -0300

    alt - get e set de Registro

--- persistencia/pRegistro.java
+++ persistencia/pRegistro.java
@@ -7,9 +7,15 @@ public class pRegistro implements iPersistencia{
 	private Registro registro;
 	
 	public pRegistro(){}
-	
-	
-	
+		
+	public Registro getRegistro() {
+		return registro;
+	}
+
+	public void setRegistro(Registro registro) {
+		this.registro = registro;
+	}
+
 	@Override
 	public boolean salvar() {
 		System.out.println("Registro salvo com sucesso");
