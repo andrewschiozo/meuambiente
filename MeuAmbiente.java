1146307ca98caf7c61393da4162e00cad40931b5
Andrews <andrewschiozo@gmail.com>
2016-06-03 16:57:18 -0300

    alt - Sequência de execução

--- MeuAmbiente.java
+++ MeuAmbiente.java
@@ -1,18 +1,75 @@
-import classes.Data;
+import classes.Endereco;
 import classes.Pessoa;
 import classes.Problema;
 import classes.Registro;
+import persistencia.pRegistro;
+import java.text.ParseException;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+
 
 public class MeuAmbiente {
 
-	public static void main(String[] args) {
-		Data andrewsData = new Data(13,01,1993);
-		Pessoa andrews = new Pessoa("Andrews", andrewsData, "39075063830");
-		Problema problema1 = new Problema("Despejo de esgoto em rio", "Tratamento do esgoto");
-		Registro registro = new Registro(andrews, problema1);
-		//System.out.println(andrews);
-		//System.out.println(problema1);
-		System.out.println(registro);
-	}
+	public static void main(String[] args) throws ParseException {
+		int cadastrar = 0;
+		int visualizarRegistros;
+		pRegistro registros = new pRegistro(); //Instância da classe que armazena os registros
 
+		while(cadastrar == 0){
+			// Declaração de variáveis
+			String mensagem1 = "Bem-vindo ao Projeto Meu Ambiente.";
+			mensagem1 += "\n\n Clique em OK para informar um problema ambiental";
+			String nome;
+			String cpf = "";
+			String cep;
+			String descricao;
+			String sugestao;
+			Problema problema = new Problema(); 	//Instância do Problema
+
+			JOptionPane.showMessageDialog(null, mensagem1); // Tela inicial
+			
+			//Coleta de dados
+			nome 	  = JOptionPane.showInputDialog("Informe seu nome");
+			cpf  	  = JOptionPane.showInputDialog("Informe seu CPF");
+			cep  	  = JOptionPane.showInputDialog("Informe o CEP do local do problema a reportar");
+			descricao = JOptionPane.showInputDialog("Conte-nos sobre o problema");
+
+			Pessoa pessoa = new Pessoa(nome, cpf); 			//Instância da Pessoa
+			Endereco enderecoProblema= new Endereco(cep);  //Instância de endereco
+			
+			problema.setEndereco(enderecoProblema);		 //  População do
+			problema.setDescricao(descricao);			//   problema
+			
+			// Opção de sugestão do usuário
+			int respostaSolucao = JOptionPane.showConfirmDialog(null, "Deseja propor uma solução?");
+			if(respostaSolucao == 0){
+				sugestao = JOptionPane.showInputDialog("Qual solução você sugere?");
+				problema.setSugestaoMelhoria(sugestao);
+			}
+			
+			
+			Registro registro = new Registro(pessoa, problema);	//Instância do registro
+			registros.setRegistro(registro); // População dos registros
+			cadastrar = JOptionPane.showConfirmDialog(null, "Deseja informar um novo problema?");
+		}
+		
+		// Opção de visualizar registros
+		visualizarRegistros = JOptionPane.showConfirmDialog(null, "Deseja visualizar os registros?");
+		if(visualizarRegistros == 0 ){
+			JFrame frame = new JFrame("App");
+			frame.setSize(320, 220);
+			
+			JLabel label = new JLabel();
+			label.setSize(300, 200);
+			label.setText(registros.toString());
+			
+			frame.add(label);
+			frame.setVisible(true);
+		}else{
+			JOptionPane.showMessageDialog(null, "Obrigado por utilizar o nosso sistema");
+		}
+	}
 }
