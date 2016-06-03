77bb958410dae0f207136b8fc24723da86bfa075
Andrews <andrewschiozo@gmail.com>
2016-06-03 16:55:04 -0300

    alt - remoção dos atributos id e dataNascimento e ajuste no método toString

--- classes/Pessoa.java
+++ classes/Pessoa.java
@@ -2,27 +2,16 @@ package classes;
 
 public class Pessoa {
 
-	private int 	id;
 	private String 	nome;
-	private Data	dataNascimento;
 	private String	cpf;
 	
-	public Pessoa(String nome, Data dataNascimento, String cpf){
+	public Pessoa(String nome, String cpf){
 		this.setNome(nome);
-		this.setDataNascimento(dataNascimento);
 		try {
 			this.setCpf(cpf);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
 	}
 	
 	public String getNome() {
@@ -31,14 +20,6 @@ public class Pessoa {
 
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-
-	public Data getDataNascimento() {
-		return dataNascimento;
-	}
-
-	public void setDataNascimento(Data dataNascimento) {
-		this.dataNascimento = dataNascimento;
 	}
 
 	public String getCpf() {
@@ -56,7 +37,7 @@ public class Pessoa {
 	
 	@Override
 	public String toString() {
-		return "Pessoa [\n\tNome: " + getNome() + "\n\tData Nasc.: " + getDataNascimento() + "\n\tCPF: " + getCpf() + "\n\t]";
+		return "Nome do cidadão: " + getNome() + "\nCPF: " + getCpf() + "\n";
 	}
 	
 }
