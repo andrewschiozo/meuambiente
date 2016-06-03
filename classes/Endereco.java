b98922775744374aaef7e6c79c0fdcba9eb0796d
Andrews <andrewschiozo@gmail.com>
2016-06-03 16:54:19 -0300

    inc - Classe genérica

---
+++ classes/Endereco.java
@@ -0,0 +1,25 @@
+package classes;
+
+public class Endereco {
+
+	private String cep;
+	
+	public Endereco(String cep){
+		this.setCep(cep);
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	@Override
+	public String toString() {
+		return "Cep da ocorrência: " + getCep() + "\n";
+	}
+	
+	
+}
